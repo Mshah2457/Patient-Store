281f3a56bb0e75c85f91ac92bcf2e4b50c00c31c
DhrutiUpadhyay <37285944+DhrutiUpadhyay@users.noreply.github.com>
2018-06-14 11:53:40 +0530

    Created dummy Dashboard Component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Dashboard extends React.Component {
+  render() {
+    return (
+      <View style={styles.inputContainer}>
+        <Text>This is Dashboard...</Text>
+      </View>
+    )
+  }
+
+};
+
+
+const styles = StyleSheet.create({
+  
+});
